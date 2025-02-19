330d7614d1857e260e470e6d26dec8f0656a333c
unknown <YY212@ccs.local>
2025-02-19 21:22:41 +0800

    2nd commit

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,26 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import cors from "cors"
-import helmet from "helmet"
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import { userRouter } from "./users/users.routes";
 
-dotenv.config()
+dotenv.config();
 
 if (!process.env.PORT) {
-    console.log(`No port value specified...`)
+    console.log(`No port value specified...`);
 }
 
-const PORT = parseInt(process.env.PORT as string, 10)
+const PORT = parseInt(process.env.PORT as string, 10);
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-app.use(cors())
-app.use(helmet())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+app.use("/", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`);
+});
